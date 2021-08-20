3193d8e1d067ed0a365c8b2b8e8777287ec8ae85
P Obi <info@aeskill.com>
2021-08-20 10:52:58 -0400

    Added REST Framework

---
+++ djangular3/djangular3/scrumboard/api.py
@@ -0,0 +1,13 @@
+from django.db.models import query
+from rest_framework.generics import ListAPIView
+
+from .serializers import ListSerializer, CardSerializer
+from .models import List, Card
+
+class ListApi(ListAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+class CardApi(ListAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
